8d5f36795e1eb8c12e4e656eda3d1d13a9da8774
Andrey Golovizin <golovizin@gmail.com>
2010-07-11 13:08:00 +0400

    Use db.sqlite3 instead of :memory: for the non-test DB.

--- testproject/settings.py
+++ testproject/settings.py
@@ -12,7 +12,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': ':memory:',                      # Or path to database file if using sqlite3.
+        'NAME': 'db.sqlite3',                      # Or path to database file if using sqlite3.
+        'TEST_NAME': ':memory:',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
