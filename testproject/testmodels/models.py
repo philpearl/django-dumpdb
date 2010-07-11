6cae128bb9136865711c40f4dae0309855ba0566
Andrey Golovizin <golovizin@gmail.com>
2010-07-11 13:07:15 +0400

    Tweak models.

--- testproject/testmodels/models.py
+++ testproject/testmodels/models.py
@@ -1,11 +1,13 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
-    birth_date = models.DateTimeField()
-    parent = models.ForeignKey('self')
+    birth_date = models.DateTimeField(default=datetime.now)
+    parent = models.ForeignKey('self', null=True)
 
 class Family(models.Model):
     members = models.ManyToManyField(Person)
